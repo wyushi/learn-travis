6e93eccad41e0ff160136678f5fb119b07abe157
Yushi Wang <jasper.ys.wang@gmail.com>
2016-06-06 15:56:56 -0600

    branch out for failed test

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,12 +3,6 @@
 var expect = require('expect');
 
 describe('test', () => {
-
-  it('should pass', (done) => {
-    expect(true).toExist();
-    done();
-  });
-
   it('let\'s me this fail', () => {
     expect(false).toExist();
     done();
